73d7f3ab487bc905ffb40652077bb08e469df140
Tsebaot Girma <tsebaotg1@gmail.com>
2024-12-25 09:24:59 +0300

    Update scripts and ignore unnecessary files

--- scripts/engagement_metrics.py
+++ scripts/engagement_metrics.py
@@ -1,12 +1,17 @@
 import pandas as pd
 
+
 def calculate_engagement_metrics(data):
     # Aggregate metrics per MSISDN
     metrics = data.groupby('MSISDN/Number').agg(
         session_frequency=('Bearer Id', 'count'),
         session_duration=('Dur. (ms)', 'sum'),
-        total_traffic=('Total DL (MB)', 'sum') + ('Total UL (MB)', 'sum')
+        total_download_traffic=('Total DL (Bytes)', 'sum'),
+        total_upload_traffic=('Total UL (Bytes)', 'sum')
     ).reset_index()
+    
+   # Calculate total traffic as a new column
+    metrics['total_traffic'] = metrics['total_download_traffic'] + metrics['total_upload_traffic']
     
     return metrics
 
@@ -16,3 +21,6 @@ def normalize_metrics(metrics):
     normalized = scaler.fit_transform(metrics[['session_frequency', 'session_duration', 'total_traffic']])
     
     return pd.DataFrame(normalized, columns=['normalized_frequency', 'normalized_duration', 'normalized_traffic'])
+
+
+
